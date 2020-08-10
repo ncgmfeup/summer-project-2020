6ef8fc3c6da13e7555650aa0a82bdebc4f37556b
Retchut <57546866+Retchut@users.noreply.github.com>
2020-08-11 00:07:03 +0100

    shooting
    Dispara no espaço.
    Tiros desaparecem após 10s, ou tocando num objeto que não tenha a layer "Player Related"

---
+++ FEUP Mayhem/Assets/Scripts/weapon/bulletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+
+    private Rigidbody2D bulletRB;
+    public float bulletForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletRB = this.GetComponent<Rigidbody2D>();
+        bulletRB.gravityScale = 0;
+        bulletRB.AddForce(transform.right * bulletForce);
+        Destroy(this.gameObject, 10f);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collided");
+        Destroy(this.gameObject);
+    }
+}
---
+++ FEUP Mayhem/Assets/Scripts/weapon/weaponController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weaponController : MonoBehaviour
+{
+    public Transform weaponTransform;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Instantiate(bulletPrefab, weaponTransform.position, weaponTransform.rotation);
+        }
+    }
+}
