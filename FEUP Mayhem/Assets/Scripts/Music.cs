f326367604a49616f801c31410485bd1e39103a1
anaines14 <anaiobarros2014@gmail.com>
2020-08-25 01:39:03 +0100

    Added mute button / Saving PlayerPrefs

---
+++ FEUP Mayhem/Assets/Scripts/Music.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    static Music instance = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+            Destroy(gameObject);    
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        
+    }
+
+    // Update is called once per frame
+    public void ToggleSound()
+    {
+        if(PlayerPrefs.GetInt("Muted", 0) == 0)
+            PlayerPrefs.SetInt("Muted", 1);
+        else
+            PlayerPrefs.SetInt("Muted", 0);
+    }
+}
