2c894a7122028323d0c60410f8919687664d87a1
joaoalc <up201806531@fe.up.pt>
2020-08-29 17:53:31 +0100

    Current ammo text implemented
    I thought I pushed this yesterday lmao

---
+++ FEUP Mayhem/Assets/Scripts/ShowAmmoNumber.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShowAmmoNumber : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI textMeshPro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateAmmoNumber(int numBullets, int clipSize, int numClips, bool autofire)
+    {
+        textMeshPro.text = numBullets.ToString();
+
+    }
+}
