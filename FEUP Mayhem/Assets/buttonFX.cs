f2741842ba231f5464c43ba5ccddcd98de9e0816
anaines14 <anaiobarros2014@gmail.com>
2020-09-07 14:29:09 +0100

    Added colors to pause menu

---
+++ FEUP Mayhem/Assets/buttonFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFX : MonoBehaviour
+{
+    static buttonFX instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != null)
+            Destroy(gameObject);
+        else{
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+}
