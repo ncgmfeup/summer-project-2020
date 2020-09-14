9c5734d1f73a6c80a8d99563c7b7908e7cea9d02
up201503447 <up201503447@fe.up.pt>
2020-09-14 14:38:22 +0100

    Reload indicator

---
+++ FEUP Mayhem/Assets/Scripts/ReloadBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadBar : MonoBehaviour
+{
+    private Vector2 pos;
+    public GameObject sprite;
+
+    private GameObject icon;
+
+    void Start(){
+        pos = new Vector2(20,40);
+        icon = Instantiate(sprite);
+    }
+
+    public void StartReloading(){
+        pos = new Vector2(transform.position.x, transform.position.y + 1);
+        icon.GetComponent<Transform>().position = pos;
+        icon.SetActive(true); 
+    }
+
+    public void EndReload(){
+        icon.SetActive(false);
+    }
+
+}
