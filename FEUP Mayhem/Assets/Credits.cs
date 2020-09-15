5f848bbfb54dcbc34f0000fbf81c24316c7da237
Pedro Esteves <up201705160@fe.up.pt>
2020-09-15 22:31:48 +0100

    Fixed credits animation

---
+++ FEUP Mayhem/Assets/Credits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fadeOut = null;
+
+    private void FadeOut()
+    {
+        fadeOut.SetActive(true);
+    }
+}
