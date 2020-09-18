5abb813bf317a349a05c06fed44c4b706dc2cded
Retchut <57546866+Retchut@users.noreply.github.com>
2020-09-18 12:33:09 +0100

    simple respawn
    will build upon it later

---
+++ FEUP Mayhem/Assets/Scripts/Player/respawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform respawnPoint;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerTransform.position = respawnPoint.position;
+    }
+}
