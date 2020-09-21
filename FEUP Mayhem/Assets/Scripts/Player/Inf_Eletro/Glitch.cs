f7f29062c8b248d5e413810959d3d82d441e330d
Pedro Esteves <up201705160@fe.up.pt>
2020-09-21 11:13:15 +0100

    Fixed Inf_Eletro controller. Added Glitch script

---
+++ FEUP Mayhem/Assets/Scripts/Player/Inf_Eletro/Glitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class Glitch : MonoBehaviour
+{
+    private Animator animator;
+
+    private float minTime = 5f, maxTime = 10f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(AnimatorGlitch());
+    }
+
+    private IEnumerator AnimatorGlitch()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+
+            animator.SetTrigger("glitch");
+        }
+    }
+
+}
