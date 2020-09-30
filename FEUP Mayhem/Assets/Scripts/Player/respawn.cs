a7ee06e9c61466ef49714eb0d76b11825c4dfd21
Mario Travassos <retchut@protonmail.ch>
2020-09-30 10:28:29 +0100

    Update respawn.cs

--- FEUP Mayhem/Assets/Scripts/Player/respawn.cs
+++ FEUP Mayhem/Assets/Scripts/Player/respawn.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class respawn : MonoBehaviour
 {
-    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private Transform respawnPoint;            //holds reference to the respawn point transform
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        StartCoroutine(WaitToMove(collision.transform));
+        StartCoroutine(WaitToMove(collision.transform));        //starts coroutine and passes the collided tranform to it
     }
 
     private IEnumerator WaitToMove(Transform collisionTransform)
     {
-        yield return new WaitForSeconds(2f);
-        collisionTransform.position = respawnPoint.position;
+        yield return new WaitForSeconds(2f);                    //waits 2 secs
+        collisionTransform.position = respawnPoint.position;    //changes collided tranform position
     }
 }
