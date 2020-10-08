8c0428a030d6437bdf5acf2c3a62dd81d67486e9
up201503447 <up201503447@fe.up.pt>
2020-10-08 14:24:47 +0100

    Added new volume sliders

--- FEUP Mayhem/Assets/Scripts/Music/SetVolume.cs
+++ FEUP Mayhem/Assets/Scripts/Music/SetVolume.cs
@@ -13,9 +13,21 @@ public class SetVolume : MonoBehaviour {
     {
         slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
     }
-    public void SetLevel (float sliderValue)
+    public void SetMasterLevel (float sliderValue)
+    {
+	    mixer.SetFloat("MasterVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MasterVolume", sliderValue);
+    }
+
+    public void SetMusicLevel (float sliderValue)
     {
 	    mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
         PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+    public void SetSFXLevel (float sliderValue)
+    {
+	    mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", sliderValue);
     }
 }
